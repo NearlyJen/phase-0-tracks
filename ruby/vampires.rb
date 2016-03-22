95a0415c7cb4db6a78b4962ff41978177d14eeb4
Jennifer Cross <nearlyjen@gmail.com>
2016-03-22 13:30:52 -0500

    ask vampire questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+print "What is your name?"
+name=gets.chomp
+
+print "How old are you?"
+age=gets.chomp
+
+print "What year were you born?"
+year_born=gets.chomp
+
+print "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+wants_bread=gets.chomp
+
+print "Would you like to enroll in the company’s health insurance? (y/n)"
+wants_insurance=gets.chomp
