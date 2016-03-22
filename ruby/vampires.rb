bb4ea84803fecdd3e5f722c48c9a4596ee19d848
Jennifer Cross <nearlyjen@gmail.com>
2016-03-22 14:28:35 -0500

    release 3 done

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,38 +1,43 @@
-print "What is your name?"
-name=gets.chomp
+currentyear = 2016
+num = 0
 
-print "How old are you?"
-age=gets.chomp.to_i
+	print "How many employees will we be processing?"
+	employees = gets.chomp.to_i
 
+while num < employees do
 
-print "What year were you born?"
-yearborn=gets.chomp.to_i
+	print "What is your name?"
+	name=gets.chomp
 
-print "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
-wantsbread=gets.chomp
-
-print "Would you like to enroll in the company’s health insurance? (y/n)"
-wantsinsurance=gets.chomp
+	print "How old are you?"
+	age=gets.chomp.to_i
 
 
-# if age is equal to current_year minus year_born && (is willing to eat garlic bread || get insurance)
-# print out "Probably not a vampire"
+	print "What year were you born?"
+	yearborn=gets.chomp.to_i
 
- currentyear = 2016
+	print "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+	wantsbread=gets.chomp
+
+	print "Would you like to enroll in the company’s health insurance? (y/n)"
+	wantsinsurance=gets.chomp
 
 
-if name == "Drake Cula" || name == "Tu Fang"
-	puts "Definitely a vampire"
+	if name == "Drake Cula" || name == "Tu Fang"
+		puts "Definitely a vampire"
 
-elsif age != (currentyear - yearborn) && (wantsbread == "n" && wantsinsurance == "n")
-	puts "Almost certainly a vampire."
+	elsif age != (currentyear - yearborn) && (wantsbread == "n" && wantsinsurance == "n")
+		puts "Almost certainly a vampire."
 
-elsif age != (currentyear - yearborn) && (wantsbread == "n" || wantsinsurance == "n")
-	puts "Probably a vampire"
+	elsif age != (currentyear - yearborn) && (wantsbread == "n" || wantsinsurance == "n")
+		puts "Probably a vampire"
 
-elsif age == (currentyear - yearborn) && (wantsbread == "y" || wantsinsurance == "y")
-	puts "Probably not a vampire"
+	elsif age == (currentyear - yearborn) && (wantsbread == "y" || wantsinsurance == "y")
+		puts "Probably not a vampire"
 
-else puts "Results inconclusive."
+	else 
+		puts "Results inconclusive."
+	end
 
+	num += 1
 end
