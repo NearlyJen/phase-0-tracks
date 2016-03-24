b31d2429051e84348fdd0a51912abdcb26940a95
Jennifer Cross <nearlyjen@gmail.com>
2016-03-24 18:53:23 -0500

    initial commit with variables

---
+++ ruby/hamsters.rb
@@ -0,0 +1,14 @@
+p "What is the name of your lovely hamster?"
+name = gets.chomp
+
+p "What is the approximate volume level of this gorgeous hamster on a scale from 1-10?"
+volume = gets.chomp
+
+p "How would you describe the fuzzy and fluffy fur color of your amazing hamster?"
+fur = gets.chomp
+
+p "Do you feel like your super-hamster is adoptable?(y/n)"
+adopt = gets.chomp
+
+p "How many years young is this freakishly youthful looking hamster?"
+age = gets.chomp
