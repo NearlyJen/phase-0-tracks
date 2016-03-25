ae50851b006c36d977f0a56360a0b4b5ad45f454
Jennifer Cross <nearlyjen@gmail.com>
2016-03-24 19:31:32 -0500

    convert to appropriate data types

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,13 +2,20 @@ p "What is the name of your lovely hamster?"
 name = gets.chomp
 
 p "What is the approximate volume level of this gorgeous hamster on a scale from 1-10?"
-volume = gets.chomp
+volume = gets.chomp.to_i
 
 p "How would you describe the fuzzy and fluffy fur color of your amazing hamster?"
 fur = gets.chomp
 
-p "Do you feel like your super-hamster is adoptable?(y/n)"
-adopt = gets.chomp
+p "True or False: Your super-hamster would make an AWESOME pet for someone else."
+adopt = gets.chomp.downcase
+
+if adopt == "true" || adopt == "yes" || adopt == "totally"
+	 adoptme = true
+else 
+	 adoptme = false
+end
+
 
 p "How many years young is this freakishly youthful looking hamster?"
-age = gets.chomp
+age = gets.chomp || nil
