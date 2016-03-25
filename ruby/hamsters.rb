b71f6914dc1e7d40d7d8c85e71cc402477919395
Jennifer Cross <nearlyjen@gmail.com>
2016-03-24 20:10:42 -0500

    add some interesting assumptions based on input

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -4,21 +4,42 @@ name = gets.chomp.capitalize
 p "What is the approximate volume level of this gorgeous hamster on a scale from 1-10?"
 volume = gets.chomp.to_i
 
+	if volume >= 5
+		volume = "really super loud while you're sleeping"
+	else
+		volume = "so quiet you think they may be dead"
+	end
+
 p "How would you describe the fuzzy and fluffy fur color of your amazing hamster?"
 fur = gets.chomp.downcase
 
 p "True or False: Your super-hamster would make an AWESOME pet for someone else."
 adopt = gets.chomp.downcase
 
-if adopt == "true" || adopt == "yes" || adopt == "totally"
-	 adoptme = true && goodpet = "awesome"
+	if adopt == "true" || adopt == "yes" || adopt == "totally"
+		 adopt = true 
 
-else 
-	 adoptme = false && goodpet = "terrible"
-end
+	elsif adopt == "false" || adopt == "nope" || adopt == "no way"
+		 adopt = false
+	end
 
+	if adopt == true
+		goodpet = "awesome" 
+		result = "adoption"
+	elsif adopt == false
+		goodpet = "terrible" 
+		result = "euthanasia"
+	end
 
 p "How many years young is this freakishly youthful looking hamster?"
 age = gets.chomp.to_i || nil
 
-p "Interesting! It sounds like #{name} with the #{fur} fur would make a(n) #{goodpet} pet. It seems like #{volume} is the perfect noise level for a #{age} year-old hamster. Let's get to work finding them a new home!"
+	if age > 5
+		age = "A hamster on the verge of death"
+	elsif age > 2
+		age = "A hamster in the prime of its life"
+	else 
+		age = "An itty bitty baby hamster"
+	end
+
+p "Interesting! It sounds like #{name} with the beautiful #{fur} fur would make a(n) #{goodpet} pet. #{age} is always #{volume}. Let's talk more about #{result}!"
