6d5e64caf927f66e76d395d97629b5703a1cff5c
Jennifer Cross <nearlyjen@gmail.com>
2016-03-28 22:54:18 -0500

    initial with questions

---
+++ ruby/hashes.rb
@@ -0,0 +1,22 @@
+# get user input for name, age, theme, and if kids
+#show the user what they have entered
+#ask if they would like to update any information
+#if they would, then ask whose data they would like to update
+#get updated data
+# print updated information
+
+client_data = { }
+
+puts "Welcome to your awesome client data storage program! What is the name of your client?"
+name = gets.chomp
+
+puts "What is their age?"
+age = gets.chomp
+
+
+puts "What is their chosen theme?"
+theme = gets.chomp
+
+puts "Do they have children? (y/n)"
+kids = gets.chomp
+
