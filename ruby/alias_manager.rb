0b45cd39b4ff40b1af72d1ae026f877899132cee
Jennifer Cross <nearlyjen@gmail.com>
2016-03-29 17:59:20 -0500

    remove if statement for immediate quit

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,7 +3,7 @@
 aliases = Hash.new
 #empty string to hold the user selection
 choice = ""
-
+real_name = " "
 # run this until they want to be done getting new names
 until choice == 'quit'
 	puts "Do you need a new alias? ('yes' or 'quit')"
@@ -36,13 +36,10 @@ until choice == 'quit'
 	
 	# when they're done, show them all of the names/aliases
 	elsif choice == 'quit'
-		if aliases[:realname] == nil
-			puts "You have no saved aliases."
-		else #print out each pair in the hash as a sentence
-			aliases.each do |real_name, alias_name|
-	    		puts "#{real_name} is known as #{alias_name}"
-	    	end
-    	end
+		#print out each pair in the hash as a sentence
+		aliases.each do |real_name, alias_name|
+	    	puts "#{real_name} is known as #{alias_name}"
+	    end
     else
     	puts "Please enter 'yes' to acquire a new alias, or 'quit' to exit this program"
     end
