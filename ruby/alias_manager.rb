b71220596ea3354eb199110721afa523fa9c3fa1
Jennifer Cross <nearlyjen@gmail.com>
2016-03-29 17:42:49 -0500

    run until user is done, print out hash

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,18 +1,46 @@
 
-#get the users real name
-puts "What is your real name?"
-real_name = gets.chomp.downcase
+#hash to store the real names and aliases
+aliases = Hash.new
+#empty string to hold the user selection
+choice = ""
 
-	#take the name and sub out the letters for new letters
-	name = real_name.tr('aeiou', 'eioua')
-	name = name.tr('bcdfghjklmnpqrstvwxyz', 'cdfghjklmnpqrstvwxyzb')
-	# make it an array and then capitalize each item in that array
-	name = name.split(' ').map( & :capitalize)
-	# reverse the things in the array
-	name = name.reverse
-	#make a string again
-	alias_name = name.join(' ')
+# run this until they want to be done getting new names
+until choice == 'quit'
+	puts "Do you need a new alias? ('yes' or 'quit')"
+	choice = gets.chomp.downcase
 
-#print it out
-puts "You shall now be known as '#{alias_name}'"
-
+	if choice == 'yes'
+		
+		#get the users real name
+		puts "What is your real name?"
+		real_name = gets.chomp
+		
+			#downcase it here so that the symbol stays capitalized
+			name = real_name.downcase
+			#take the name and sub out the letters for new letters
+			name = name.tr('aeiou', 'eioua')
+			name = name.tr('bcdfghjklmnpqrstvwxyz', 'cdfghjklmnpqrstvwxyzb')
+			
+			# make it an array and then capitalize each item in that array
+			name = name.split(' ').map( & :capitalize)
+			
+			# reverse the things in the array
+			name = name.reverse
+			
+			#make a string again
+			alias_name = name.join(' ')
+			aliases[real_name.to_sym] = alias_name
+		
+		#print it out
+		puts "You shall now be known as '#{alias_name}'"
+	
+	# when they're done, show them all of the names/aliases
+	elsif choice == 'quit'
+		#print out each pair in the hash as a sentence
+		aliases.each do |real_name, alias_name|
+    		puts "#{real_name} is known as #{alias_name}"
+    	end
+    else
+    	puts "Please enter 'yes' to acquire a new alias, or 'quit' to exit this program"
+    end
+end
