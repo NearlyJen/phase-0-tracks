3392a40fc2343db7d36e197501cf7685f840684d
Jennifer Cross <nearlyjen@gmail.com>
2016-03-29 15:37:16 -0500

    nested data structures added

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,28 @@
+cross_family = {
+
+	jennifer: {
+		middle_name: "Lynn",
+		kidney_count: 1,
+		sports: ["rowing", "kung-fu", "crossfit"]
+
+	},
+
+	theron: {
+		middle_name: "Andrew",
+		kidney_count: 2,
+		sports: ["swimming", "kung-fu", "crossfit"]
+
+	},
+
+	ethan:  {
+		middle_name: "Samuel",
+		kidney_count: 2,
+		sports: ["running", "kung-fu", "soccer"]
+
+	}
+}
+
+
+cross_family[:jennifer][:middle_name].downcase.reverse
+cross_family[:ethan][:sports][2]
+cross_family[:theron][:kidney_count].to_s
