233c88caf7d158b46cce767396495d5c84bfe57a
Jennifer Cross <nearlyjen@gmail.com>
2016-03-28 23:41:33 -0500

    still working, still ugly

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -5,6 +5,8 @@
 #get updated data
 # print updated information
 
+#note, i know everything that is repeated should go in a method but I can't quite work out how to do it yet. 
+
 client_data = { }
 
 puts "Welcome to your awesome client data storage program! What is the name of your client?"
@@ -20,44 +22,45 @@ theme = gets.chomp
 client_data[theme] = theme
 
 puts "Do they have children? (y/n)"
-kids = gets.chomp
+kids = gets.chomp.downcase
+kids = kids == 'y' ? true : false
 client_data[kids] = kids
 
 choice = " "
 until choice == "no" 
-puts "You've added #{name}, age #{age} who wants #{theme} and #{kids}. Would you like to update this information?"
-choice = gets.chomp
+	puts "You've added #{name}, age #{age} who wants #{theme} and that they responded '#{kids}' to having children. Would you like to update this information?"
+	choice = gets.chomp
 
-if choice == "yes"
-	puts "Would you like to update the name, age, theme, or if they have children?"
-	update = gets.chomp
-	case update
-	when "name"
-		puts "What is the new name of your client?"
-		name = gets.chomp
-		client_data[name] = name.to_sym
-		puts "You've updated the name to #{name}"
-	when "age"
-		puts "What is their age?"
-		age = gets.chomp
-		client_data[age] = age.to_i
-		puts "You've updated the age to #{age}"
-	when "theme"
-		puts "What is their chosen theme?"
-		theme = gets.chomp
-		client_data[theme] = theme
-		puts "You've updated the theme to #{theme}"
-	when "kids"
-		puts "Do they have children? (y/n)"
-		kids = gets.chomp
-		client_data[kids] = kids
-		puts "You've updated their kids status to #{kids}"
-	else
-		puts "Please enter a valid field"
+	if choice == "yes"
+		puts "Would you like to update the name, age, theme, or 'kids'?"
+		update = gets.chomp
+		case update
+		when "name"
+			puts "What is the new name of your client?"
+			name = gets.chomp
+			client_data[name] = name.to_sym
+			puts "You've updated the name to #{name}"
+		when "age"
+			puts "What is their age?"
+			age = gets.chomp
+			client_data[age] = age.to_i
+			puts "You've updated the age to #{age}"
+		when "theme"
+			puts "What is their chosen theme?"
+			theme = gets.chomp
+			client_data[theme] = theme
+			puts "You've updated the theme to #{theme}"
+		when "kids"
+			puts "Do they have children? (y/n)"
+			kids = gets.chomp
+			client_data[kids] = kids
+			puts "You've updated their kids status to #{kids}"
+		else
+			puts "Please enter a valid field"
+		end
+	else choice == "no" || nil
+		puts "Awesome. Have a great day!"
 	end
-else choice == "no" || nil
-	puts "Awesome. Have a great day!"
-end
 end
 
 
