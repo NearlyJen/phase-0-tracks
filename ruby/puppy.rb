f28a7138f7553d67df0391ed2d54a5e3f9289fb8
Jennifer Cross <nearlyjen@gmail.com>
2016-04-04 16:08:31 -0500

    puppies! add description and make class

---
+++ ruby/puppy.rb
@@ -0,0 +1,25 @@
+# SPECIES ------------------------
+# Great Dane Puppy
+
+# CHARACTERISTICS ----------------
+# Legs: long
+# Ears: natural
+# Tail: long
+# Name varies
+# Color: varies
+# Age: < 2 years
+
+# BEHAVIOR -----------------------
+# Sleep
+# Eat
+# Drool
+# Poop
+
+class Puppy
+
+end
+
+fern = Puppy.new
+barney = Puppy.new
+otis = Puppy.new
+
