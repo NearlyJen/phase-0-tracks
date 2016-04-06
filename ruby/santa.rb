b0e1bb17ea2487df797b24cda502b7074130c3f0
Jennifer Cross <nearlyjen@gmail.com>
2016-04-06 11:11:24 -0500

    santas release 2 complete

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -15,11 +15,35 @@ class Santa
   def eat_milk_and_cookies(cookie_type)
     puts "That was a good #{cookie_type}!"
   end
+
+  def celebrate_birthday
+    @age += 1
+    puts "Santa is now #{@age}! Happy Birthday!"
+  end
+
+  def get_mad_at(reindeer_name)
+    @reindeer_ranking.delete(reindeer_name)
+    @reindeer_ranking.insert(-1, reindeer_name)
+    puts @reindeer_ranking
+  end
+
+  #getter methods for attributes
+  def gender
+    @gender
+  end
+
+  def ethnicity
+    @ethnicity
+  end
 end
 
 jen = Santa.new('female', 'multi')
 jen.speak
 jen.eat_milk_and_cookies('ginger snap')
+jen.gender
+jen.ethnicity
+jen.get_mad_at('Dasher')
+jen.celebrate_birthday
 
 santas = []
 gender_options = ['boy', 'girl', 'man', 'woman', 'trans', 'undefined', 'fluid']
@@ -28,4 +52,5 @@ ethnicity_options = ['white', 'black', 'brown', 'pinkish', 'greenish', 'purplish
 gender_options.length.times do |x|
   santas << Santa.new(gender_options[x], ethnicity_options[x])
 end
+
 
