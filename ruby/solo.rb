8734e33b253601f926bde549c7ed951f04c0eafc
Jennifer Cross <nearlyjen@gmail.com>
2016-04-06 14:15:55 -0500

    release 0 - design class

---
+++ ruby/solo.rb
@@ -0,0 +1,10 @@
+# Release 0 - Design a class
+# class WarcraftToon - this will build a new character for plaing in WoW
+# will be either Alliance or Horde
+# will be male or female
+# can choose from lists of both classes and races
+# will have a level 1-100
+# methods:
+# fight or run and hide depending on level
+# dance - makes your toon break into dance
+# quest - go looking for things to do!
