0226f17aac75b42850095a8a360855de4d74dc9a
Jennifer Cross <nearlyjen@gmail.com>
2016-04-06 10:46:20 -0500

    complete release 1

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,13 @@
 # builds class for Santas
 class Santa
+  def initialize(gender, ethnicity)
+    puts 'Initializing Santa instance ...'
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ['Rudolph', 'Dasher', 'Dancer', 'Prancer', 'Vixen', 'Comet', 'Cupid', 'Donner', 'Blitzen']
+    @age = 0
+  end
+
   def speak
     puts 'Ho Ho Ho! Haaaaapy Holidays!'
   end
@@ -7,12 +15,17 @@ class Santa
   def eat_milk_and_cookies(cookie_type)
     puts "That was a good #{cookie_type}!"
   end
-
-  def initialize
-    puts 'Initializing Santa instance ...'
-  end
 end
 
-jen = Santa.new
+jen = Santa.new('female', 'multi')
 jen.speak
 jen.eat_milk_and_cookies('ginger snap')
+
+santas = []
+gender_options = ['boy', 'girl', 'man', 'woman', 'trans', 'undefined', 'fluid']
+ethnicity_options = ['white', 'black', 'brown', 'pinkish', 'greenish', 'purplish', 'dalak']
+
+gender_options.length.times do |x|
+  santas << Santa.new(gender_options[x], ethnicity_options[x])
+end
+
