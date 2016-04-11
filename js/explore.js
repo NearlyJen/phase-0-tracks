93a91694e6328b45ba127272bfd0adf4e8520f0c
Jennifer Cross <nearlyjen@gmail.com>
2016-04-11 15:58:15 -0500

    new pseudocode for new plan

--- js/explore.js
+++ js/explore.js
@@ -6,11 +6,20 @@
 // join the letters back together again into a new string
 // return the new string
 
+// new plan
+// declare a function that takes a string
+// going in reverse
+// for each letter in the string loop through one time
+// and add that letter to a new string
+// when there are no more letters to add
+//return the new string
+
 function reverse(str) {
   var reversed_str = '';
-  for (var i = str.length - 1; i >= 0; i--)
+  for (var i = str.length - 1; i >= 0; i--) {
     reversed_str += str[i];
-    return reversed_str
+  }
+    return reversed_str;
 }
 
   if (1 == 1){
