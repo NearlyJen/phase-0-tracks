83db73b0c7bea596713a039e11b8560c6798975a
Jennifer Cross <nearlyjen@gmail.com>
2016-04-14 18:50:52 -0500

    lame release 1 solution added

--- js/algos.js
+++ js/algos.js
@@ -19,6 +19,36 @@ function whatsLongest(my_Array) {
   return longestLongest;
 }
 
+
+// make a function that takes two objects
+// loop through the objects and compare
+// to see if any of object 1's key-value pairs are equal to
+// object 2's key-value pairs
+// return true if the loop finds one
+// implemented lame-o solution because i can't work out the loop
+
+var obj1 = {age: 42, name: "Jennifer", weight: 175};
+var obj2 = {age: 44, name: "Theron", weight: 175};
+
+function doTheyMatch (obj1, obj2) {
+    if (obj1.age == obj2.age) {
+      return true;
+    }
+     else if (obj1.name == obj2.name){
+      return true;
+     }
+     else if (obj1.weight == obj2.weight) {
+      return true;
+     }
+    else {
+      return false;
+    }
+}
+
+
+
+doTheyMatch(obj1, obj2);
+
 console.log(whatsLongest(["Jennifer", "Theron", "Ethan", "Barbarasam", "Sam"]));
 
 console.log(whatsLongest(["Joe", "Nietzsche", "Buddy", "Fern"]));
